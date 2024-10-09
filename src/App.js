50f33fdfae2f6068b12782751bab1a18ae6e8bbd
anirudhsaroha <anirudhsaroha01@gmail.com>
2024-10-09 14:28:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./pages/Navbar";
+import Landing from "./pages/Landing";
+import About from "./pages/About";
+import MySkills from "./pages/MySkills";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Footer from "./pages/Footer";
+import LineGradient from "./components/LineGradient";
+import { useState } from "react";
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState('home');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app  bg-deep-blue">
+      <Navbar  selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <div className="w-5/6 mx-auto md:h-full">
+        <Landing setSelectedPage={setSelectedPage} />
+      </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto md:h-full">
+        <About />
+      </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto md:h-full">
+        <MySkills />
+      </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto">
+        <Projects />
+      </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto">
+        <Contact />
+      </div>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
